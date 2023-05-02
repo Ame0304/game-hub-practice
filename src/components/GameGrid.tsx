b43110ae97474065b7e5bd8d1109407508b98602
Ame0304 <rhj903265106@gmail.com>
2023-05-01 22:13:07 -0400

    Display platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         spacing={10}
-        padding={10}
+        padding="10px"
       >
         {games.map((game) => (
           <GameCard key={game.id} game={game}></GameCard>
